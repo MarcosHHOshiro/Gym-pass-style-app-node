a208d4effb6396200352de30001292f7458b3bc2
marcos <marcos.2h.oshiro@gmail.com>
2026-01-29 16:57:50 -0400

    5:43 checkins

---
+++ src/use-cases/checkins.ts
@@ -0,0 +1,34 @@
+import type { UsersRepository } from "@/repositories/users-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-erros";
+import { compare } from "bcryptjs";
+import type { CheckIn } from "generated/prisma/client";
+import type { CheckInsRepository } from "@/repositories/check-ins-repository";
+
+interface CheckInUseCaseRequest {
+    userId: string;
+    gymId: string;
+}
+
+interface CheckInUseCaseResponse {
+    checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+    constructor(
+        private checkInsRepository: CheckInsRepository
+    ) { }
+
+    async execute({
+        userId,
+        gymId
+    }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const checkIn = await this.checkInsRepository.create({
+            gym_id: gymId,
+            user_id: userId
+        });
+
+        return {
+            checkIn
+        }
+    }
+}
