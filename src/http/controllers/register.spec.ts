0f098ce352d847d19da645a7b3efb3db0e8da0ae
marcos <marcos.2h.oshiro@gmail.com>
2026-02-04 13:34:29 -0400

    test e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,29 @@
-import { expect, test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { app } from '@/app';
+import request from 'supertest';
+import { prisma } from '@/lib/prisma';
 
-test('testing', () => {
-    expect(true).toBe(true);
+describe('Register (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready();
+    })
+
+    afterAll(async () => {
+        await app.close();
+    })
+
+    it('should be able to register', async () => {
+
+        const result = await prisma.$queryRawUnsafe(`select current_schema() as schema`)
+        console.log(result)
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name: 'John Doe',
+                email: 'john6.doe@example.com',
+                password: '123456'
+            });
+
+        expect(response.statusCode).toEqual(201);
+    })
 })
